84574ef69b50d297aef62bca07956a542c70c0a7
lossyrob <rdemanuele@gmail.com>
2015-08-18 16:35:36 -0400

    Allow activities to be passed in explicitly (e.g. by package)

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 "use strict";
 
-var activity = require("./lib/activity"),
+var activities = require("./lib/activities"),
+    activity = require("./lib/activity"),
     decider = require("./lib/decider"),
     output = require("./lib/output"),
     shell = require("./lib/shell");
 
+module.exports.activities = activities;
 module.exports.activity = activity;
 module.exports.decider = decider;
 module.exports.output = output;
